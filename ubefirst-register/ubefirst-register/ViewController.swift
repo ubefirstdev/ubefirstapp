6651c2c20631f129044f2916f6c633ecdc319eb2
Román Falcón <roman_fg97@hotmail.com>
2018-10-11 11:56:33 -0500

    Commit inicial
    Aqui estan los primeros assests y los dos views para inicio de sesion y registro.

---
+++ ubefirst-register/ubefirst-register/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ubefirst-register
+//
+//  Created by MariaJose De la Fuente on 11/10/18.
+//  Copyright © 2018 roman. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
