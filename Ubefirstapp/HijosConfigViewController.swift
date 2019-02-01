b60f13dee83b3eee68310d94a5c4c3abca0a8778
luis oliva <luisalbertolivamedina@hotmail.com>
2019-01-31 22:36:56 -0600

    se elimino codigo basura

--- Ubefirstapp/HijosConfigViewController.swift
+++ Ubefirstapp/HijosConfigViewController.swift
@@ -173,18 +173,3 @@ class HijosConfigViewController: UIViewController, UITableViewDataSource, UITabl
 
 }
 
-
-
-public extension UIView {
-    
-    func shake(count : Float = 4,for duration : TimeInterval = 0.5,withTranslation translation : Float = 5) {
-        let animation : CABasicAnimation = CABasicAnimation(keyPath: "transform.translation.x")
-        animation.timingFunction = CAMediaTimingFunction(name: CAMediaTimingFunctionName.linear)
-        animation.repeatCount = count
-        animation.duration = duration/TimeInterval(animation.repeatCount)
-        animation.autoreverses = true
-        animation.fromValue = NSValue(cgPoint: CGPoint(x: CGFloat(-translation), y: self.center.y))
-        animation.toValue = NSValue(cgPoint: CGPoint(x: CGFloat(translation), y: self.center.y))
-        layer.add(animation, forKey: "shake")
-    }
-}
